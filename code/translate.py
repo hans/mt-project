7c83cddee4d4a2683cfe2ac3afd71a136f3306e4
Hans Engel <engel@engel.uk.to>
2014-02-25 02:57:19 -0800

    Move evaluation code into separate file

---
+++ code/translate.py
@@ -0,0 +1,31 @@
+import itertools
+import os.path
+import sys
+
+from translator.direct import DirectTranslator
+
+
+def main():
+    if len(sys.argv) != 3:
+        print "Usage: 'python translate.py original_text.txt dictFile.txt'"
+        return
+    if not os.path.isfile(sys.argv[1]):
+        print "Original text file is invalid"
+        return
+    if not os.path.isfile(sys.argv[2]):
+        print "Dictionary file is invalid"
+        return
+
+    translators = [DirectTranslator]
+
+    for translator_class in translators:
+        translator = translator_class(sys.argv[2])
+        sentences = translator.translate_sentences(sys.argv[1])
+
+        print '----- %s' % translator_class
+        for sentence in sentences:
+            print ' '.join(next(itertools.product(*sentence)))
+
+
+if __name__ == '__main__':
+    main()
--- code/translator/direct.py
+++ code/translator/direct.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-
-import sys
-import os
-import itertools
-
 from core import Translator
 
 class DirectTranslator(Translator):
@@ -15,25 +9,3 @@ class DirectTranslator(Translator):
     def translate(self, sentence):
         return [self.dictionary.get(word.lower(), [word])
                 for word in sentence]
-
-
-def main():
-    if len(sys.argv) != 3:
-        print "Usage: 'python translator.py original_text.txt dictFile.txt'"
-        return
-    if not os.path.isfile(sys.argv[1]):
-        print "Original text file is invalid"
-        return
-    if not os.path.isfile(sys.argv[2]):
-        print "Dictionary file is invalid"
-        return
-
-    translator = DirectTranslator(sys.argv[2])
-    sentences = translator.translate_sentences(sys.argv[1])
-
-    for sentence in sentences:
-        print ' '.join(next(itertools.product(*sentence)))
-
-
-if __name__ == "__main__":
-    main()
