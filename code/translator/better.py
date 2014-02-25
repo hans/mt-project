f166f0804ce724b1dfdb1bdedc95475ef531ffa1
Hans Engel <engel@engel.uk.to>
2014-02-25 03:01:15 -0800

    Create "BetterTranslator" boilerplate

---
+++ code/translator/better.py
@@ -0,0 +1,24 @@
+"""Defines a translator that is, er, better than the baseline. Hence the
+name.
+
+TODO: remove awkwardness"""
+
+
+from core import Translator
+
+
+class BetterTranslator(Translator):
+    def preprocess(self, sentence):
+        """Given a list of tokens from a source language sentence,
+        preprocess the tokens. Returns a tuple of the form
+        `(preprocessed_sentence, annotations)`
+
+        TODO: describe annotations result"""
+
+        return sentence, []
+
+    def translate(self, sentence):
+        sentence, annotations = self.preprocess(sentence)
+
+        return [self.dictionary.get(word.lower(), [word])
+                for word in sentence]
