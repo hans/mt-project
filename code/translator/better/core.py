8cd8128ac4136291d4008da05a3e0a711caef95a
Hans Engel <engel@engel.uk.to>
2014-02-28 10:20:01 -0800

    Kill prepositional homicide

--- code/translator/better/core.py
+++ code/translator/better/core.py
@@ -36,7 +36,7 @@ class BetterTranslator(DirectTranslator):
 
     POSTPROCESSING_PIPELINE = [
         postprocessing.fix_demonstratives,
-        gramm_expand.prep_homicide,
+        # gramm_expand.prep_homicide,
         gramm_expand.gramm_expand,
         postprocessing.fix_an,
         postprocessing.fix_dont,
